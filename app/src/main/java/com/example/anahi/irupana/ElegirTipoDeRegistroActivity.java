088b5e624bf70d60ae0eaf2299ca57c4ec0715a9
sabrinaquiroga <sabriquiroga@yahoo.es>
2017-03-06 01:31:07 -0400

    registros

--- app/src/main/java/com/example/anahi/irupana/ElegirTipoDeRegistroActivity.java
+++ app/src/main/java/com/example/anahi/irupana/ElegirTipoDeRegistroActivity.java
@@ -1,14 +1,51 @@
 package com.example.anahi.irupana;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class ElegirTipoDeRegistroActivity extends Activity {
+public class ElegirTipoDeRegistroActivity extends AppCompatActivity {
+
+    private Button btnCliente;
+    private Button btnTienda;
+    private Context context;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elegir_tipo_de_registro);
+
+        context=this;
+        btnCliente=(Button)findViewById(R.id.btnCliente);
+        btnTienda=(Button)findViewById(R.id.btnTienda);
+
+
+        //evento de hacer click en el boton de cliente
+        btnTienda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(context,RegistroDeTiendaActivity.class);
+                startActivity(a);
+            }
+        });
+
+        btnCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(context,RegistroDeClienteActivity.class);
+                startActivity(a);
+            }
+        });
+
+
     }
+
 
 }
--- app/src/main/java/com/example/anahi/irupana/RegistroDeClienteActivity.java
+++ app/src/main/java/com/example/anahi/irupana/RegistroDeClienteActivity.java
@@ -1,8 +1,38 @@
 package com.example.anahi.irupana;
 
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by Ing. Rafael Serate on 04/03/17.
  */
 
-public class RegistroDeClienteActivity {
+public class RegistroDeClienteActivity extends AppCompatActivity{
+    private Button btnCrearCuentaComoCliente;
+    private Context context;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_de_cliente);
+        context=this;
+        btnCrearCuentaComoCliente=(Button)findViewById(R.id.btnCrearCuentaComoCliente);
+
+
+        btnCrearCuentaComoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent a=new Intent(context,MenuPrincipal.class);
+                startActivity(a);
+            }
+        });
+
+    }
+
+
+
 }
