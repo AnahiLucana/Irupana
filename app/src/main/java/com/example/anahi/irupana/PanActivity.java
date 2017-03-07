42aaf177462f96ec4c5f9bd9e5ba0312ee69d040
CamiloSimon <camihu8@gmail.com>
2017-03-06 22:24:48 -0400

    lista de panes

--- app/src/main/java/com/example/anahi/irupana/PanActivity.java
+++ app/src/main/java/com/example/anahi/irupana/PanActivity.java
@@ -2,15 +2,28 @@ package com.example.anahi.irupana;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Ing. Rafael Serate on 04/03/17.
  */
 
 public class PanActivity extends AppCompatActivity {
+
+    ListView ListView;
+
+    String[] valores = new String[]{"Pan Chamillo","Pan de semillas","Pan de Quinua","Pan de soya","Pan de Anis","Pan de avena","pan de Wilkaparu","pan molde"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pan);
+
+        ListView = (ListView)findViewById(R.id.lista);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_expandable_list_item_1,valores);
+        ListView.setAdapter(adapter);
+
     }
 }
