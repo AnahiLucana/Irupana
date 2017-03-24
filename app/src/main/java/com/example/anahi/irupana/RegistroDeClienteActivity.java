648ccc76f95594cfa7fde2f8d4b31a87b732dfb7
CamiloSimon <camihu8@gmail.com>
2017-03-23 20:31:04 -0400

    arreglado

--- app/src/main/java/com/example/anahi/irupana/RegistroDeClienteActivity.java
+++ app/src/main/java/com/example/anahi/irupana/RegistroDeClienteActivity.java
@@ -14,7 +14,7 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
-public class RegistroDeClienteActivity extends AppCompatActivity{
+public class RegistroDeClienteActivity extends AppCompatActivity {
 
     private Button btnCrearCuentaComoCliente;
     private Context context;
@@ -30,15 +30,15 @@ public class RegistroDeClienteActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_de_cliente);
-        context=this;
-        btnCrearCuentaComoCliente=(Button)findViewById(R.id.btnCrearCuentaComoCliente);
-        txtCNombre=(EditText)findViewById(R.id.txtCNombre) ;
-        txtCApellido=(EditText)findViewById(R.id.txtCApellido) ;
-        txtCEmail=(EditText)findViewById(R.id.txtCEmail) ;
-        txtCPassword=(EditText)findViewById(R.id.txtCPassword);
-        txtCRepetirPassword=(EditText)findViewById(R.id.txtCRepetirPassword);
-        txtCNit=(EditText)findViewById(R.id.txtCNit);
-        DialogAnimado=(Button)findViewById(R.id.btnCrearCuentaComoCliente);
+        context = this;
+        btnCrearCuentaComoCliente = (Button) findViewById(R.id.btnCrearCuentaComoCliente);
+        txtCNombre = (EditText) findViewById(R.id.txtCNombre);
+        txtCApellido = (EditText) findViewById(R.id.txtCApellido);
+        txtCEmail = (EditText) findViewById(R.id.txtCEmail);
+        txtCPassword = (EditText) findViewById(R.id.txtCPassword);
+        txtCRepetirPassword = (EditText) findViewById(R.id.txtCRepetirPassword);
+        txtCNit = (EditText) findViewById(R.id.txtCNit);
+        DialogAnimado = (Button) findViewById(R.id.btnCrearCuentaComoCliente);
 
         btnCrearCuentaComoCliente.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,52 +50,51 @@ public class RegistroDeClienteActivity extends AppCompatActivity{
                 String campo_repetir_password = txtCRepetirPassword.getText().toString();
                 String campo_nit = txtCNit.getText().toString();
 
-                guardarEnDb(campo_nombre,campo_apellido,campo_password,campo_email,campo_nit);
+                guardarEnDb(campo_nombre, campo_apellido, campo_password, campo_email, campo_nit);
 
                 AlertDialog dialogo;
                 AlertDialog.Builder builder = new AlertDialog.Builder(RegistroDeClienteActivity.this);
 
-                if (campo_nombre.isEmpty()&& campo_apellido.isEmpty()&& campo_nit.isEmpty() && campo_password.equals(campo_repetir_password)
-                        &&campo_email.isEmpty()) {
+                if (campo_nombre.isEmpty() && campo_apellido.isEmpty() && campo_nit.isEmpty() && campo_password.equals(campo_repetir_password)
+                        && campo_email.isEmpty()) {
 
                     builder.setMessage("El registro no se realizo correctamente");
-                    dialogo=builder.create();
+                    dialogo = builder.create();
                     dialogo.getWindow().getAttributes().windowAnimations = R.style.transicion;
                     dialogo.show();
 
+                } else {
+                    builder.setTitle(campo_nombre);
+                    builder.setMessage("Tu registro se realizo exitosamente");
+                    dialogo = builder.create();
+                    dialogo.getWindow().getAttributes().windowAnimations = R.style.transicion;
+                    dialogo.show();
+                    dialogo.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                        public void onCancel(DialogInterface dialog) {
+                            dialog.dismiss();
+                        }
+                    });
+                    Intent a = new Intent(context, MenuPrincipal.class);
+                    startActivity(a);
+
                 }
-                else{
-                        builder.setTitle(campo_nombre);
-                        builder.setMessage("Tu registro se realizo exitosamente");
-                        dialogo = builder.create();
-                        dialogo.getWindow().getAttributes().windowAnimations = R.style.transicion;
-                        dialogo.show();
-                        dialogo.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                            public void onCancel(DialogInterface dialog) {
-                                dialog.dismiss();
-                            }
-                        });
-                        Intent a = new Intent(context, MenuPrincipal.class);
-                        startActivity(a);
-
-                    }
-
-
 
 
             }
 
-    private void guardarEnDb(String nombre, String apellido, String password, String email, String nit){
-        ContentValues values=new ContentValues();
-        values.put("nombre",nombre);
-        values.put("apellido",apellido);
-        values.put("password",password);
-        values.put("email",email);
-        values.put("nit",nit);
+            private void guardarEnDb(String nombre, String apellido, String password, String email, String nit) {
+                ContentValues values = new ContentValues();
+                values.put("nombre", nombre);
+                values.put("apellido", apellido);
+                values.put("password", password);
+                values.put("email", email);
+                values.put("nit", nit);
 
-        Base_datos baseDatos=new Base_datos(context,1);
-        SQLiteDatabase db=baseDatos.getWritableDatabase();
-        db.insert("usuarios",null,values);
+                Base_datos baseDatos = new Base_datos(context, 1);
+                SQLiteDatabase db = baseDatos.getWritableDatabase();
+                db.insert("usuarios", null, values);
+            }
+
+        });
     }
-
 }
