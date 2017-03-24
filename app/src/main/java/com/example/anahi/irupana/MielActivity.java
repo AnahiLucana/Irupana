e64573402558a474ffde51688f672d0261530112
AnahiLucana <merylv15@gmail.com>
2017-03-23 22:29:24 -0400

    o

--- app/src/main/java/com/example/anahi/irupana/MielActivity.java
+++ app/src/main/java/com/example/anahi/irupana/MielActivity.java
@@ -1,24 +1,56 @@
 package com.example.anahi.irupana;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 /**
  * Created by Ing. Rafael Serate on 04/03/17.
  */
 
 public class MielActivity extends AppCompatActivity{
+
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_miel);
+            context=this;
+
+            ListView lista=(ListView)findViewById(R.id.lista);
+            final ArrayList<Producto> items = new ArrayList<Producto>();
+
+            Producto MielDelValle = new Producto();
+            
+
+
+            Producto MielDeYungas= new Producto();
+
+            items.add(MielDelValle);
+            items.add(MielDeYungas);
 
 
 
+        }
 
 
 
-
-
-    }
 }
+
+
+
+
+
+
+
+
